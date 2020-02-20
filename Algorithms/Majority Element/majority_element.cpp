c69c56e32023dd2dd79460e62752fc2a23bb8ed6
Yusuf997 <Mohamedyouseif@outlook.com>
2020-02-20 04:16:12 +0200

    solving majority_element prob with recursion

--- Algorithms/Majority Element/majority_element.cpp
+++ Algorithms/Majority Element/majority_element.cpp
@@ -37,7 +37,51 @@ int get_majority_element(vector<long int> &a, int left, int right) {
   else
     return -1;
 }
+int get_major_elem(vector<long int> &a, int left, int right, int LSM, int RSM) {
+  if((LSM == -1) && (RSM == -1))
+  {
+    return -1;
+  }
+  else
+  {
+    int len = right - left;
+    int lsm_c = 0, rsm_c = 0;
+    for(int i = left; i < right; i++)
+    {
+      if(a[i] == LSM)
+        lsm_c += 1;
+      else if (a[i] == RSM)
+        rsm_c += 1;
+    }
 
+    if(lsm_c > len/2)
+      return LSM;
+    else if (rsm_c > len/2)
+      return RSM;
+    else
+      return -1;    
+  }
+  
+}
+
+int get_majority_element_r(vector<long int> &a, int left, int right) {
+
+  if(left < right)
+  {
+    int mid = left + (right-left)/2;
+    int LSM = get_majority_element_r(a, left, mid);
+    int RSM = get_majority_element_r(a, mid+1, right);
+    int FME = get_major_elem(a, left, right, LSM, RSM);
+
+    return FME;
+  }
+  else
+  {
+    return a[left];
+  }
+  
+
+}
 int main() {
   int n;
   std::cin >> n;
@@ -45,5 +89,5 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+  std::cout << (get_majority_element_r(a, 0, a.size()) != -1) << '\n';
 }
